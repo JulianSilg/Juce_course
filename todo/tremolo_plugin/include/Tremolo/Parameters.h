cf95c834cbffb41eccc8eb8647352c8958679658
JulianSilg <julian.silguero@gmail.com>
2026-02-17 00:41:10 -0300

    curso hasta 4.05 . bypass, smoothing transition, lfo waveform with triangle

--- todo/tremolo_plugin/include/Tremolo/Parameters.h
+++ todo/tremolo_plugin/include/Tremolo/Parameters.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <juce_audio_processors/juce_audio_processors.h>
+
 
 namespace tremolo {
 struct Parameters {
   explicit Parameters(juce::AudioProcessor&);
+
+  juce::AudioParameterFloat& rate;
+  juce::AudioParameterBool& bypassed;
+  juce::AudioParameterChoice& waveform;
+
+  JUCE_DECLARE_NON_COPYABLE(Parameters);
+  JUCE_DECLARE_NON_MOVEABLE(Parameters);
+
+
 };
 }  // namespace tremolo
--- todo/tremolo_plugin/source/Parameters.cpp
+++ todo/tremolo_plugin/source/Parameters.cpp
@@ -1,10 +1,47 @@
 
-namespace tremolo {
+  namespace tremolo {
+  namespace {
+    auto& addParameterToProcessor (juce::AudioProcessor& processor, auto parameter) {
+      auto& parameterReference = *parameter;
+      processor.addParameter(parameter.release());
+      return parameterReference;
+    }
+    juce::AudioParameterFloat& createModulationRateParameter(juce::AudioProcessor& processor) {
+      constexpr auto versionHint = 1;
+      auto parameter = std::make_unique<juce::AudioParameterFloat>(
+        juce::ParameterID{"modulatio.rate", versionHint},
+          "Modulation rate",
+          juce::NormalisableRange<float>{0.1f, 20.0f, 0.01f, 0.4f, },
+            5.0f,
+          juce::AudioParameterFloatAttributes{}.withLabel("Hz"));
+      return addParameterToProcessor(processor, std::move (parameter));
+    }
+
+    juce::AudioParameterBool& createBypassedParameter(juce::AudioProcessor& processor) {
+      constexpr auto versionHint = 1;
+      auto parameter = std::make_unique<juce::AudioParameterBool>(
+        juce::ParameterID{"bypassed", versionHint},
+        "Bypassed",
+        false);
+      return addParameterToProcessor(processor, std::move (parameter));
+    }
+
+    juce::AudioParameterChoice& createWaveformParameter(juce::AudioProcessor& processor) {
+      constexpr auto versionHint = 1;
+      auto parameter = std::make_unique<juce::AudioParameterChoice>(
+        juce::ParameterID{"modulation.waveform", versionHint},
+        "modulation.waveform",
+        juce::StringArray {"Sine", "Triangle"},
+        0);
+    return addParameterToProcessor(processor, std::move(parameter));
+    }
+  }
+
 Parameters::Parameters(juce::AudioProcessor& processor)
-// TODO: create parameters
-// TODO: retrieve references to parameters
-// TODO: add parameters to the processor
+
+  : rate{createModulationRateParameter(processor)},
+    bypassed {createBypassedParameter(processor)},
+    waveform {createWaveformParameter(processor)}
 {
-  juce::ignoreUnused(processor);
 }
 }  // namespace tremolo
