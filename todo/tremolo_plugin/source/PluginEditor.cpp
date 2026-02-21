46ab83acd8e4e7a80ecfac98d3b6104756137bac
JulianSilg <julian.silguero@gmail.com>
2026-02-20 22:13:04 -0300

    Principio de GUI (vid6), guardar parametros terminado

--- todo/tremolo_plugin/source/PluginEditor.cpp
+++ todo/tremolo_plugin/source/PluginEditor.cpp
@@ -3,11 +3,16 @@ PluginEditor::PluginEditor(PluginProcessor& p) : AudioProcessorEditor(&p) {
   background.setImage(juce::ImageCache::getFromMemory(
       assets::Background_png, assets::Background_pngSize));
 
-  logo.setImage(
-      juce::ImageCache::getFromMemory(assets::Logo_png, assets::Logo_pngSize));
+ auto logoImage = juce::ImageCache::getFromMemory(assets::Logo_png, assets::Logo_pngSize);
+  logo.setImage(logoImage);
+  logoCenter.setImage(logoImage);
+  logoRight.setImage(logoImage);
+
 
   addAndMakeVisible(background);
   addAndMakeVisible(logo);
+  addAndMakeVisible(logoCenter);
+  addAndMakeVisible(logoRight);
 
   // Make sure that before the constructor has finished, you've set the
   // editor's size to whatever you need it to be.
@@ -17,8 +22,14 @@ PluginEditor::PluginEditor(PluginProcessor& p) : AudioProcessorEditor(&p) {
 void PluginEditor::resized() {
   const auto bounds = getLocalBounds();
 
+    DBG(bounds.toString());
+
   background.setBounds(bounds);
 
   logo.setBounds({16, 16, 105, 24});
+  logoCenter.setBounds({bounds.getCentreX() - (105 / 2), 16, 105, 24});
+  logoRight.setBounds({bounds.getWidth() - 105 - 16, 16, 105, 24});
+
+
 }
 }  // namespace tremolo
